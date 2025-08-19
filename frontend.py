b332d9a00147b543399d82553f85a566702aa34f
JMDairy <jmdairydevelopment@gmail.com>
2025-08-19 17:03:11 +0530

    feat: Add analytics function to database

--- frontend.py
+++ frontend.py
@@ -9,7 +9,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from scrapers.fmp_scraper import get_indian_stocks
 
-st.set_page_config(page_title="Stock Market Scraper by Malav", page_icon="📈", layout="wide")
+st.set_page_config(page_title="Market Pulse Analyzer by Malav", page_icon="📈", layout="wide")
 API_URL = "http://127.0.0.1:8000"
 
 if 'ticker' not in st.session_state:
@@ -102,3 +102,11 @@ try:
         st.info("No results found in the database for this ticker yet.")
 except requests.ConnectionError:
     st.error("Connection Error: Could not connect to the backend API. Is it running?")
+
+    # --- FOOTER SECTION ---
+
+st.markdown("---")
+
+st.markdown("Developed by Malav | Follow on X: @malavmrmr(https://x.com/malavmrmr)")
+
+st.markdown("Powered by Streamlit & FastAPI | Data sourced from NewsAPI.org, FMP, and others.")
